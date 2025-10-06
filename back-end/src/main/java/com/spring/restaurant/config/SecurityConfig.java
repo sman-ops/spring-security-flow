b41d5ad6423dc9f00aa64c876ab5a805206fbeaf
sman-ops <slimen.ghenimi@gmail.com>
2025-10-06 22:01:18 +0100

    add users to memory with more details

--- back-end/src/main/java/com/spring/restaurant/config/SecurityConfig.java
+++ back-end/src/main/java/com/spring/restaurant/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.spring.restaurant.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -37,12 +43,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 	}
 	
-	@Override
+	/*@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("slimen").password("12345").authorities("admin")
 			.and().withUser("test").password("00000").authorities("player")
 			 .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
 		
-	}
+	}  */
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		 InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+	        UserDetails userDetails_1 = User.withUsername("islam").password("12345").authorities("admin").build();
+	        UserDetails userDetails_2 = User.withUsername("ahmed").password("00000").authorities("player").build();
+	        inMemoryUserDetailsManager.createUser(userDetails_1);
+	        inMemoryUserDetailsManager.createUser(userDetails_2);
+	        auth.userDetailsService(inMemoryUserDetailsManager);
+	}  
+	
+	
+	 @Bean
+	 public PasswordEncoder passwordEncoder(){
+	      return NoOpPasswordEncoder.getInstance();
+	   }
 
 }

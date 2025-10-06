ff8295d11aa48811a2e98aa0e34d3c015dc8883d
sman-ops <slimen.ghenimi@gmail.com>
2025-10-06 20:52:20 +0100

    config Apis with spring security

--- back-end/src/main/java/com/spring/restaurant/config/SecurityConfig.java
+++ back-end/src/main/java/com/spring/restaurant/config/SecurityConfig.java
@@ -1,5 +1,34 @@
 package com.spring.restaurant.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/*
+		http.authorizeRequests((requests) ->
+		requests.anyRequest().authenticated());
+		http.formLogin();
+		http.httpBasic(); 
+		
+		*/
+		/*
+		  http.authorizeRequests().anyRequest().permitAll()
+        .and().formLogin().and().httpBasic();  */
+		
+		http.authorizeRequests()
+		.antMatchers("/football/start").authenticated()
+		.antMatchers("/basketball/start").authenticated()
+		.antMatchers("/subscribers/start").authenticated()
+		.antMatchers("/swimming/start").authenticated()
+		.antMatchers("/about/start").permitAll()
+		.antMatchers("/connect/start").permitAll()
+	    .and().formLogin().and().httpBasic();
+		
+	}
 
 }

1277870a593c4a602393859f72ec14e1bd2d3bce
sman-ops <slimen.ghenimi@gmail.com>
2025-10-06 21:41:45 +0100

    add users in memory like user by default by spring security

--- back-end/src/main/java/com/spring/restaurant/config/SecurityConfig.java
+++ back-end/src/main/java/com/spring/restaurant/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.spring.restaurant.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,20 +21,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		/*
 		  http.authorizeRequests().anyRequest().permitAll()
         .and().formLogin().and().httpBasic();  */
-		
+		/*
 		http.authorizeRequests().anyRequest().denyAll()
         .and().formLogin().and().httpBasic();
-		
-		/*
-		http.authorizeRequests()
-		.antMatchers("/football/start").authenticated()
-		.antMatchers("/basketball/start").authenticated()
-		.antMatchers("/subscribers/start").authenticated()
-		.antMatchers("/swimming/start").authenticated()
-		.antMatchers("/about/start").permitAll()
-		.antMatchers("/connect/start").permitAll()
-	    .and().formLogin().and().httpBasic();
 		*/
+		
+		http.authorizeRequests()
+		.antMatchers("/football/*").authenticated()
+		.antMatchers("/basketball/*").authenticated()
+		.antMatchers("/subscribers/*").authenticated()
+		.antMatchers("/swimming/*").authenticated()
+		.antMatchers("/about/*").permitAll()
+		.antMatchers("/connect/*").permitAll()
+	    .and().formLogin().and().httpBasic();
+		
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("slimen").password("12345").authorities("admin")
+			.and().withUser("test").password("00000").authorities("player")
+			 .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+		
 	}
 
 }
